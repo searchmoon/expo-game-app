3a44611ab0ed505afb1aace547cdaeb65f0ddf8c
Rumi <moonding901212@gmail.com>
2024-01-14 16:25:11 +0900

    화면회전에 따라 동적으로 style 설정하기

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,23 +1,19 @@
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, Dimensions } from "react-native";
 
-import Title from '../components/ui/Title';
-import PrimaryButton from '../components/ui/PrimaryButton';
-import Colors from '../constants/colors';
+import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../constants/colors";
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
   return (
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
       <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/success.png')}
-        />
+        <Image style={styles.image} source={require("../assets/images/success.png")} />
       </View>
       <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{' '}
-        rounds to guess the number{' '}
-        <Text style={styles.highlight}>{userNumber}</Text>.
+        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the
+        number <Text style={styles.highlight}>{userNumber}</Text>.
       </Text>
       <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
@@ -26,34 +22,36 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
 
 export default GameOverScreen;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     padding: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
-    overflow: 'hidden',
+    overflow: "hidden",
     margin: 36,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   summaryText: {
-    fontFamily: 'open-sans',
+    fontFamily: "open-sans",
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 24,
   },
   highlight: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     color: Colors.primary500,
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,45 +1,52 @@
-import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import { useState } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  Dimensions,
+  ScrollView,
+  useWindowDimensions,
+} from "react-native";
 
-import PrimaryButton from '../components/ui/PrimaryButton';
-import Title from '../components/ui/Title';
-import Colors from '../constants/colors';
-import Card from '../components/ui/Card';
-import InstructionText from '../components/ui/InstructionText';
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
 function StartGameScreen({ onPickNumber }) {
-  const [enteredNumber, setEnteredNumber] = useState('');
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const { width, height } = useWindowDimensions();
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
   }
 
   function resetInputHandler() {
-    setEnteredNumber('');
+    setEnteredNumber("");
   }
 
   function confirmInputHandler() {
     const chosenNumber = parseInt(enteredNumber);
 
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-      Alert.alert(
-        'Invalid number!',
-        'Number has to be a number between 1 and 99.',
-        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
-      );
+      Alert.alert("Invalid number!", "Number has to be a number between 1 and 99.", [
+        { text: "Okay", style: "destructive", onPress: resetInputHandler },
+      ]);
       return;
     }
 
     onPickNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 300 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
-        <InstructionText>
-          Enter a Number
-        </InstructionText>
+        <InstructionText>Enter a Number</InstructionText>
         <TextInput
           style={styles.numberInput}
           maxLength={2}
@@ -67,8 +74,7 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
-    alignItems: 'center',
+    alignItems: "center",
   },
   numberInput: {
     height: 50,
@@ -78,11 +84,11 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     color: Colors.accent500,
     marginVertical: 8,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   buttonsContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   buttonContainer: {
     flex: 1,
